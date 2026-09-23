b9bc1c80372cc5f9d3a73b44d7e6b9b1f135e190
ijnebism <bjqian0201@gmail.com>
2026-09-23 21:55:25 +1200

    feat: added SFML to project as a renderer

--- engine/App.cpp
+++ engine/App.cpp
@@ -1,5 +1,20 @@
 #include "App.hpp"
 
 void App::run() {
-	std::cout << "App is running!" << std::endl;
+    sf::RenderWindow window(sf::VideoMode({ 200, 200 }), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
 }
